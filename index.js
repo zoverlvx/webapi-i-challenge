4745db9671984b19ec7c4343e372a560bd12ba27
Zach Overhulser <zoverhulser@gmail.com>
2019-05-06 19:44:45 +0000

    Runs server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+const PORT = process.env.PORT;
+
+const server = express();
+
+// tells express to read json objects
+server.use(express.json());
+
+server.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
